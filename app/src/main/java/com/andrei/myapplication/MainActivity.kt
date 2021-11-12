f9788db6bf2979592f540f0b28cf2ef707d8aa48
AndreiM07 <andreim7@mail.ru>
2021-11-12 23:00:35 +0300

    Style textView and intent

--- app/src/main/java/com/andrei/myapplication/MainActivity.kt
+++ app/src/main/java/com/andrei/myapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.andrei.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val buttonOne: Button = findViewById(R.id.button_one)
+        val intent=Intent (this, SecondActivity::class.java)
+
+        buttonOne.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
